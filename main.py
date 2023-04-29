cb009c09c24b78cb2199091cb66ac87f131b8f69
luchoaquino <luis.gonzales.a@uni.pe>
2023-04-29 14:37:15 -0500

    1er avance

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtWidgets, uic
+
+class MiVentana(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui_files/diseñochvre.ui', self)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MiVentana()
+    window.show()
+    app.exec_()
