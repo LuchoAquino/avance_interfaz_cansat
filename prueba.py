c7af488ef4da6bb36ba6549be3e041288e556800
luchoaquino <luis.gonzales.a@uni.pe>
2023-04-30 11:58:50 -0500

    Añadiendo alguito pa q se note xd

--- prueba.py
+++ prueba.py
@@ -2,3 +2,4 @@ from matplotlib import pyplot as plt
 print("Esto es una prueba que subiré a github")
 print("Haciendo time")
 print("Estamos en la rama de luchoAquino")
+print("Un cambiex para q se note xd")
